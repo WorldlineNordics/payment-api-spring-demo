590f28da66911e7ff66cba24240f790c6f9161d9
Nitin Chavhan <nchavhan@worldlinenordics.com>
2017-11-09 13:27:12 +0100

    removed validations for cardNo and cv code

--- src/main/resources/static/merchant-script.js
+++ src/main/resources/static/merchant-script.js
@@ -16,57 +16,6 @@ for(var i = 0; i <= 10; i++) {
     selectExp.appendChild(option);
 }
 
-cardNo.addEventListener("keydown", function (event) {
-	if(event.which == 32){
-		event.preventDefault();
-	}         
-});
-
-cvc.addEventListener("keydown", function (event) {
-	if(event.which == 32){
-		event.preventDefault();
-	}         
-});
-
-//check card no length    
-function validateCardNo() {	
-	var cards = new Array();
-    cards[0] = { cardName: "VISA", regEx: "^4[0-9]{12}([0-9]{3})?$"};
-    cards[1] = { cardName: "MASTERCARD", regEx: "^5[1-5][0-9]{14}$"};
-    cards[2] = { cardName: "AMEX", regEx: "^3[47][0-9]{13}$"};
-    cards[3] = { cardName: "DINERS", regEx: "^3(0[0-5]|[68][0-9])[0-9]{11}$"};
-    cards[4] = { cardName: "DISCOVER", regEx: "^6011[0-9]{12}$"};
-    var cardNum = cardNo.value;
-    var cardName = null;
-    cardNum = cardNum.replace(/[^0-9]/g,'');
-    if(cardNo){
-	    for (var i=0; i< cards.length; i++) {
-	      if (new RegExp(cards[i]["regEx"]).test(cardNum)) {
-	        cardName =  cards[i]["cardName"];  
-	        break;
-	      }
-	    }
-	    if(cardName != null){
-	    	validCard = true;					
-	    }else{
-	    	validCard = false;
-	    	alert("Invalid card number..Please provide valid card number");
-	    }
-    }
-}
-
-//check card no length
-function validateCvc() {
-	if(cvc){
-		if(cvc.value.match(/^[0-9]{3,4}$/)){
-			validCvc = true;
-		}else{
-			validCvc = false;
-			alert("Invalid CV Code..Please provide valid CV Code of 3 or 4 digits");			
-		}
-	}
-}
-
 window.addEventListener("load", function () {	    
     // Access the form element...
     var form = document.getElementById("paymentForm");
@@ -74,13 +23,7 @@ window.addEventListener("load", function () {
     // ...and take over its submit event.
     form.addEventListener("submit", function (event) {
     	event.preventDefault();
-    	// validate card no and cv code...    	
-    	if(validCard==true && validCvc==true){
-	        sendDataToMerchant();
-    	}else{
-    		validateCardNo();
-        	validateCvc();
-    	}
+    	sendDataToMerchant();    	
     });
     
     function sendDataToMerchant() {    	
