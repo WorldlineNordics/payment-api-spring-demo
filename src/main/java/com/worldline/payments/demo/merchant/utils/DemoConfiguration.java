fbedc127d869ef0df69b74898feb2350af6063fa
Payal Ghadge <pghadge@worldlinenordics.com>
2017-12-05 09:28:07 +0100

    added property source

--- src/main/java/com/worldline/payments/demo/merchant/utils/DemoConfiguration.java
+++ src/main/java/com/worldline/payments/demo/merchant/utils/DemoConfiguration.java
@@ -2,32 +2,31 @@ package com.worldline.payments.demo.merchant.utils;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource(value = { "classpath:application.properties" })
 public class DemoConfiguration {
 
+	@Value("${worldline.api.keystore.path}")
+	public String keystorePath;
 
-    @Value("${worldline.api.keystore.path}")
-    public String keystorePath;
+	@Value("${worldline.api.keystore.pwd}")
+	public String keystorePwd;
 
-    @Value("${worldline.api.keystore.pwd}")
-    public String keystorePwd;
+	@Value("${worldline.api.key.merchantkeyalias}")
+	public String merchantKeyAlias = "merchant";
 
-    @Value("${worldline.api.key.merchantkeyalias}")
-    public String merchantKeyAlias = "merchant";
+	@Value("${worldline.api.key.worldlinekeyalias}")
+	public String worldlineKeyAlias = "ngcert";
 
-    @Value("${worldline.api.key.worldlinekeyalias}")
-    public String worldlineKeyAlias = "ngcert";
+	@Value("${worldline.api.merchantId}")
+	public String merchantId;
 
-    @Value("${worldline.api.merchantId}")
-    public String merchantId;
+	@Value("${worldline.api.posId}")
+	public String posId;
 
-    @Value("${worldline.api.posId}")
-    public String posId;
-
-    @Value("${worldline.api.url}")
-    public String worldlineURL;
-
+	@Value("${worldline.api.url}")
+	public String worldlineURL;
 
 }
