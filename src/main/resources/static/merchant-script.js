4d1bc5877f8dd8740a667bc29b6f14154751c099
Rickard Schoultz <rschoultz@digitalriver.com>
2017-12-06 23:26:09 +0100

    Refactored to private scope of worldline script.

--- src/main/resources/static/merchant-script.js
+++ src/main/resources/static/merchant-script.js
@@ -1,5 +1,5 @@
-// Pick the chd elements from the form (card, cvv, expdate)
-var chdElements = document.querySelectorAll('[chd]');
+// Pick the data-chd elements from the form (card, cvv, expdate)
+var chdElements = document.querySelectorAll('[data-chd]');
 var cardHolderName = chdElements[0];
 var cardNo = chdElements[1];
 var expMonth = chdElements[2];
@@ -8,7 +8,7 @@ var cvc = chdElements[4];
 var validCard = false, validCvc = false;
 var year = (new Date()).getFullYear(), selectExp = expYear, option = null, next_year = null;
 
-for(var i = 0; i <= 10; i++) {
+for (var i = 0; i <= 10; i++) {
     option = document.createElement("option");
     next_year = parseInt(year, 10) + i;
     option.value = next_year;
@@ -16,61 +16,75 @@ for(var i = 0; i <= 10; i++) {
     selectExp.appendChild(option);
 }
 
-window.addEventListener("load", function () {	    
+window.addEventListener("load", function () {
     // Access the form element...
     var form = document.getElementById("paymentForm");
-            
+
     // ...and take over its submit event.
     form.addEventListener("submit", function (event) {
-    	event.preventDefault();
-    	displayResult('...','');
-    	sendDataToMerchant();
+        event.preventDefault();
+        displayResult('...', '');
+        sendDataToMerchant();
     });
-    
-    function sendDataToMerchant() {    	
-        var XHR = new XMLHttpRequest();
-        
+
+    function sendDataToMerchant() {
+//        var XHR = new XMLHttpRequest();
+        var XHR = createRequest();
+
         // Bind the FormData object and the form element
         var FD = new FormData(form);
 
         // Define what happens on successful data submission
-        XHR.addEventListener("load", function() {
-        	if (this.readyState == 4 && this.status == 200) {
-	        	var response = JSON.parse(this.responseText);
-	            // Call worldline-script.js method that calls Device REST API
-	            sendPayment(sendResultToUnpack,showError,response.encryptedPayload,response.path,cardHolderName.value,cardNo.value,expMonth.value,expYear.value,cvc.value);
-        	}        	
+
+        XHR.addEventListener("load", function () {
+            if (this.readyState == 4 && this.status == 200) {
+                var response = JSON.parse(this.responseText);
+                // Call drwp-script.js method that calls Device REST API
+
+                new PaymentRequest()
+                    .cardHolderName(cardHolderName.value)
+                    .cardNumber(cardNo.value)
+                    .expDateMonth(expMonth.value)
+                    .expDateYear(expYear.value)
+                    .cvCode(cvc.value)
+                    .encryptedPayload(response.encryptedPayload)
+                    .path(response.path)
+                    .onSuccess(sendResultToUnpack)
+                    .onError(showError)
+                    .send
+                //sendPayment(sendResultToUnpack, showError, response.encryptedPayload, response.path, cardHolderName.value, cardNo.value, expMonth.value, expYear.value, cvc.value);
+            }
         });
 
         // Define what happens in case of error
-        XHR.addEventListener("error", function() {
-        	displayResult('','Status :'+this.status+' '+this.statusText +' Oups.! Something goes wrong.');
+        XHR.addEventListener("error", function () {
+            displayResult('', 'Status :' + this.status + '. ' + this.statusText);
         });
-                
+
         // Set up our request
         XHR.open("POST", "/api/users/registrations");
 
         // The data sent is what the user provided in the form
         XHR.send(FD);
-    }  
-    
+    }
+
 });
 
 function sendResultToUnpack(transactionResult) {
     var xhttp = new XMLHttpRequest();
-        
+
     // Define what happens when response recieved successfully
-    xhttp.addEventListener("load", function() {
+    xhttp.addEventListener("load", function () {
         if (this.readyState == 4 && this.status == 200) {
-            var res =  JSON.parse(this.responseText);            
+            var res = JSON.parse(this.responseText);
             console.log(res);
             displayResult("Status : "+ res.status+"<br>TransactionId : " + res.transactionId+"<br>OrderId : " + res.orderId, "");
        }
-    });
+   });
 
     // Define what happens in case of error
-    xhttp.addEventListener("error", function() {
-    	displayResult("", 'Status: '+this.status+'<br>'+this.statusText);
+    xhttp.addEventListener("error", function () {
+        displayResult("", 'Status: ' + this.status + '<br>' + this.statusText);
     });
 
     // Set up our request
@@ -78,12 +92,30 @@ function sendResultToUnpack(transactionResult) {
 
     // The data sent is what the user provided in the form
     xhttp.send(transactionResult);
-};
+}
+function createRequest() {
+    var request;
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+        request = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE
+        try {
+            request = new ActiveXObject('Msxml2.XMLHTTP');
+        }
+        catch (e) {
+            try {
+                request = new ActiveXObject('Microsoft.XMLHTTP');
+            }
+            catch (e) {
+            }
+        }
+    }
+
+    return request;
+}
 
 function showError(error) {
-      displayResult("", "Error :" + error);
-};
-
+    displayResult("", "Error :" + error);
+}
 function displayResult(result, error) {
     document.getElementById("payment-result").innerHTML = result;
     document.getElementById("payment-errors").innerHTML = error;
--- src/main/resources/static/worldline-script.js
+++ src/main/resources/static/worldline-script.js
@@ -1,41 +1,101 @@
-function sendPayment(success, error, encryptedPayload, path, cardHolderName, cardNumber, expDateMonth, expDateYear, cvCode) {
-	
-    var xhttp = new XMLHttpRequest();
-       
-    // Bind the encrypted object and the elements
-    var data = JSON.stringify({cardHolderName : cardHolderName,
-    						   cardNumber : cardNumber,
-    						   expDateMonth : expDateMonth,
-    						   expDateYear : expDateYear,
-    						   cvCode : cvCode,
-    						   encryptedPayload : encryptedPayload});
-	
-    // Define what happens when response recieved successfully
-    xhttp.addEventListener("load", function() {
-    	if (this.readyState == 4 && this.status == 201) {
-    		var result = JSON.parse(this.responseText);
-            // Call method that calls API to unpack response
-    		success(result.encResponse);
-    	}
-    	
-    	// Define what happens in case of error
-    	if (this.status == 400 || this.status == 401) {
-    		var result =  this.responseText;
-            // Call method that handles the error
-    		error(result);
-    	}
-    	
-    	if (this.status == 405 ) {
-      		displayResult('','Status : '+this.status+' '+', Please verify the Worldline API URL');
-     	}
-    });
-                   
-    // Set up our request
-    xhttp.open("POST", path ,true);
-   
-    // Set content type as json
-    xhttp.setRequestHeader("Content-type", "application/json");
-    
-    // The data sent is what the user provided in the form
-    xhttp.send(data);
-};
+
+    function sendPayment(success, error, encryptedPayload, path, cardHolderName, cardNumber, expDateMonth, expDateYear, cvCode) {
+
+        var xhttp = new XMLHttpRequest();
+
+        var data = JSON.stringify({
+            cardHolderName: cardHolderName,
+            cardNumber: cardNumber,
+            expDateMonth: expDateMonth,
+            expDateYear: expDateYear,
+            cvCode: cvCode,
+            encryptedPayload: encryptedPayload
+        });
+
+        xhttp.addEventListener("load", function () {
+            if (this.readyState === 4 && this.status === 201) {
+                success(JSON.parse(this.responseText).encResponse);
+            }
+
+            // Define what happens in case of error
+            if (this.status === 400 || this.status === 401) {
+                error(this.responseText);
+            }
+
+            if (this.status === 405) {
+                error('Status : ' + this.status + ' ' + ', Please verify the Worldline API URL');
+            }
+        });
+
+        xhttp.addEventListener("timeout", function () {
+            error('Timeout while processing transaction.');
+        });
+
+        xhttp.open("POST", path, true);
+        xhttp.timeout = 60000;
+        xhttp.setRequestHeader("Content-type", "application/json");
+        xhttp.send(data);
+    }
+
+    var PaymentRequest = function () {
+        var _theForm, _cardHolderName, _cardNumber, _expDateMonth, _expDateYear, _cvCode, _encryptedPayload, _path;
+        var _success, _error;
+        var _status;
+
+        var _self = {
+            cardHolderName: function (n) {
+                _cardHolderName = n;
+                return this
+            },
+            form: function (n) {
+                _theForm = n;
+                return this
+            },
+            cardNumber: function (n) {
+                _cardNumber = n;
+                return this
+            },
+            expDateMonth: function (n) {
+                _expDateMonth = n;
+                return this
+            },
+            expDateYear: function (n) {
+                _expDateYear = n;
+                return this
+            },
+            cvCode: function (n) {
+                _cvCode = n;
+                return this
+            },
+            encryptedPayload: function (n) {
+                _encryptedPayload = n;
+                return this
+            },
+            path: function (n) {
+                _path = n;
+                return this
+            },
+            onSuccess: function (n) {
+                _success = n;
+                return this
+            },
+            onError: function (n) {
+                _error = n;
+                return this
+            },
+            send: function() {
+                _status = sendPayment(_success, _error, _encryptedPayload, _path, _cardHolderName, _cardNumber, _expDateMonth, _expDateYear, _cvCode);
+            }
+
+        };
+
+        Object.defineProperty(_self, "status", {
+            get: function () {
+                return _status;
+            }
+        });
+
+        return _self;
+    };
+
+    console.log("HEJ");
