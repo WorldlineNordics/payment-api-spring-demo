fec6e1daac78d7e9bfda458fc2a4ef6dc6c1acba
Rickard Schoultz <rschoultz@digitalriver.com>
2017-12-07 16:37:05 +0100

    Remove IE XHR

--- src/main/resources/static/merchant-script.js
+++ src/main/resources/static/merchant-script.js
@@ -88,25 +88,6 @@ function sendResultToUnpack(transactionResult) {
     xhttp.send(transactionResult);
 }
 
-function createRequest() {
-    var request;
-    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-        request = new XMLHttpRequest();
-    } else if (window.ActiveXObject) { // IE
-        try {
-            request = new ActiveXObject('Msxml2.XMLHTTP');
-        }
-        catch (e) {
-            try {
-                request = new ActiveXObject('Microsoft.XMLHTTP');
-            }
-            catch (e) {
-            }
-        }
-    }
-    return request;
-}
-
 function showError(error) {
     console.log("Merchant Server returned error.");
     displayResult("", "Error :" + error);
