366318dee50d80427d877da8a913fc79897f140d
Radha Patil <rpatil@worldlinenordics.com>
2019-01-10 13:15:19 +0100

    EXSPRING-42:Ibp result page changes

--- src/main/resources/static/ibp-result.js
+++ src/main/resources/static/ibp-result.js
@@ -2,7 +2,41 @@
 
 
 window.addEventListener("load", function () {
-    
-    });
-    
+	var url = new URL(window.location.href);
+	var response = url.searchParams.get("response");
+	console.log(response);
+	unPackResponse(response);
 });
+
+function unPackResponse(response){
+    	 makeRequest({
+        	method: 'POST',
+        	url: '/api/demo/unpackResponse',
+        	encode: true,
+        	params: JSON.stringify(response)
+    	})
+	
+     .then(function (unpackedResponse) {
+        response = JSON.parse(unpackedResponse);
+    	 console.log(response);
+        displayResult("Status: " + response.status
+            + "<br>TransactionId: " + response.transactionId
+            + "<br>OrderId: " + response.orderId
+            + "<br>Payment Method: " + response.paymentMethodName
+            , "");
+    })
+    .catch(function (err) {
+     showError(err);
+    });
+	
+}
+
+function displayResult(result, error) {
+    document.getElementById("payment-result").innerHTML = result;
+    document.getElementById("payment-errors").innerHTML = error;
+}
+
+function showError(error) {
+    console.error(error);
+    displayResult("", "Error :" + error.status + ": " + error.statusText);
+}
