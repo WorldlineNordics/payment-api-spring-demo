995b0391575dc1c55c1c00fba83096ab844502d5
Bhargav Madia <bmadia@worldlinenordics.com>
2019-01-15 12:52:51 +0100

    redirect changes-cookies

--- src/main/resources/static/ibp-result.js
+++ src/main/resources/static/ibp-result.js
@@ -5,8 +5,35 @@ window.addEventListener("load", function () {
 	var url = new URL(window.location.href);
 	var response = url.searchParams.get("response");
 	console.log(response);
-	unPackResponse(response);
+	if(response){
+		unPackResponse(response);
+	}
+	else{
+		var form = document.createElement('form');
+		form.setAttribute('method','GET');
+		form.setAttribute('action','http://localhost:1082/ibpcp');
+
+		var cookies = document.cookie.split(';');
+
+		var pp1 = getCookie('pp1');
+		var el = document.createElement('input');
+
+		el.setAttribute('type','hidden');
+		el.setAttribute('name','pp');
+		el.setAttribute('value',pp1);
+
+		form.appendChild(el);
+
+		document.body.appendChild(form);
+		form.submit()
+	}
 });
+
+function getCookie(name){
+	var value = "; " + document.cookie;
+	var parts = value.split("; " + name + "=");
+	if (parts.length == 2) return parts.pop().split(";").shift();
+}
 
 function unPackResponse(response){
     	 makeRequest({
