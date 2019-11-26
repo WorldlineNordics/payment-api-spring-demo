00da192ebad9f94f487ef33724bd4b696af95b22
Surbhi Agrawal <sagrawal@worldlinenordics.com>
2019-11-26 10:07:12 +0100

    EXSPRING-125 Internal to external code merge

--- src/main/resources/static/demo.js
+++ src/main/resources/static/demo.js
@@ -1,166 +1,180 @@
-
-async function processAuthentication(formAsJson) {
-	try {
-		displayResult("Processing Initiate Authentication with Worldline.", "");
-		var initResponse = await service.initAuth().chdForm(document.getElementById("card_details"), 'data-chd').send();
-		if (initResponse.tDSMethodContent) {
-			processTDSMethodContent(initResponse.tDSMethodContent);
-			await timeout(3000);
-		}
-		if ('AUTHENTICATION' == initResponse.status && ('NOT_REQUIRED' == initResponse.authenticationStatus || 'SUCCESSFUL' == initResponse.authenticationStatus)) {
-			//proceed with existing payment and display result
-			displayResult("Authentication Successful. Proceed with Payment", "");
-			processCardAfterAuthentication(deviceAPIRequest, initResponse.worldlineSessionData);
-		} else if ('AUTHENTICATION' === initResponse.status && 'CONTINUE' === initResponse.authenticationStatus) {
-			displayResult("Processing Continue Authentication with Worldline.", "");
-			var continueResponse = await service.continueAuth().setWorldlineSessionData(initResponse.worldlineSessionData).send();
-			if ('AUTHENTICATION' === continueResponse.status && 'SUCCESSFUL' === continueResponse.authenticationStatus) {
-				//process with payment and display result
-				displayResult("Authentication Successful. Proceed with Payment", "");
-				processCardAfterAuthentication(deviceAPIRequest, continueResponse.worldlineSessionData);
-			} else if ('AUTHENTICATION' == continueResponse.status && 'REQUIRED' == continueResponse.authenticationStatus) {
-				//rediect to ACS using iFrame
-				displayResult("Processing Complete Authentication with Worldline.", "");
-				processAuthenticationRedirect(continueResponse);
-			} else {
-				//Don't proceed for payment flow and show appropriate message to user.
-				unpackResponse(continueResponse);
-			}
-		} else if ('AUTHENTICATION' == initResponse.status && 'REQUIRED' == initResponse.authenticationStatus) {
-			//rediect to ACS using iFrame
-			displayResult("Processing Complete Authentication with Worldline.", "");
-			processAuthenticationRedirect(initResponse);
-		} else {
-			//Don't proceed for payment flow and show appropriate message to user.
-			unpackResponse(initResponse);
-		}
-	}
-	catch (err) {
-		showError(err);
-	}
+async function processAuthentication() {
+  try {
+    displayResult('Processing Initiate Authentication with Worldline.', '');
+    var initResponse = await service
+      .initAuth()
+      .chdForm(document.getElementById('card_details'), 'data-chd')
+      .send();
+    if (initResponse.tDSMethodContent) {
+      processTDSMethodContent(initResponse.tDSMethodContent);
+      await timeout(3000);
+    }
+    if (
+      'AUTHENTICATION' == initResponse.status &&
+      ('NOT_REQUIRED' == initResponse.authenticationStatus || 'SUCCESSFUL' == initResponse.authenticationStatus)
+    ) {
+      //proceed with existing payment and display result
+      displayResult('Authentication Successful. Proceed with Payment', '');
+      processCardAfterAuthentication(deviceAPIRequest, initResponse.worldlineSessionData);
+    } else if ('AUTHENTICATION' === initResponse.status && 'CONTINUE' === initResponse.authenticationStatus) {
+      displayResult('Processing Continue Authentication with Worldline.', '');
+      var continueResponse = await service
+        .continueAuth()
+        .setWorldlineSessionData(initResponse.worldlineSessionData)
+        .send();
+      if ('AUTHENTICATION' === continueResponse.status && 'SUCCESSFUL' === continueResponse.authenticationStatus) {
+        //process with payment and display result
+        displayResult('Authentication Successful. Proceed with Payment', '');
+        processCardAfterAuthentication(deviceAPIRequest, continueResponse.worldlineSessionData);
+      } else if ('AUTHENTICATION' == continueResponse.status && 'REQUIRED' == continueResponse.authenticationStatus) {
+        //rediect to ACS using iFrame
+        displayResult('Processing Complete Authentication with Worldline.', '');
+        processAuthenticationRedirect(continueResponse);
+      } else {
+        //Don't proceed for payment flow and show appropriate message to user.
+        unpackResponse(continueResponse);
+      }
+    } else if ('AUTHENTICATION' == initResponse.status && 'REQUIRED' == initResponse.authenticationStatus) {
+      //rediect to ACS using iFrame
+      displayResult('Processing Complete Authentication with Worldline.', '');
+      processAuthenticationRedirect(initResponse);
+    } else {
+      //Don't proceed for payment flow and show appropriate message to user.
+      unpackResponse(initResponse);
+    }
+  } catch (err) {
+    showError(err);
+  }
 }
 
 async function processCardAfterAuthentication(deviceAPIRequest, worldlineSessionData) {
-	displayResult("Processing Payment with Worldline.", "");
-	var response = await service.cardPayment().setWorldlineSessionData(worldlineSessionData).chdForm(document.getElementById("card_details"), 'data-chd').send();
-	displayResult("Processing result with merchant.", "");
-	unpackResponse(response);
+  displayResult('Processing Payment with Worldline.', '');
+  var response = await service
+    .cardPayment()
+    .setWorldlineSessionData(worldlineSessionData)
+    .chdForm(document.getElementById('card_details'), 'data-chd')
+    .send();
+  displayResult('Processing result with merchant.', '');
+  unpackResponse(response);
 }
 
-async function processCard() {
-	formAsJson = formToJson(form);
-	var response = await makeRequest({
-		method: 'POST',
-		url: '/api/demo/registrations',
-		encode: false,
-		params: JSON.stringify(formAsJson)
-	})
-	deviceAPIRequest = JSON.parse(JSON.parse(response).deviceAPIRequest);
-	service = new Worldline.PaymentService(deviceAPIRequest);
-	try {
-		displayResult("Processing Payment with Worldline.", "");
-		var response = await service.cardPayment().chdForm(document.getElementById("card_details"), 'data-chd').setRequestTimeout(requestTimeout).send();
-		displayResult("Processing result with merchant.", "");
-		unpackResponse(response);
-	}
-	catch (err) {
-		showError(err);
-	}
+async function processCard(cardObj) {
+  try {
+    displayResult('Processing Payment with Worldline.', '');
+    var response = await service
+      .cardPayment()
+      .card(cardObj)
+      .send();
+    displayResult('Processing result with merchant.', '');
+    unpackResponse(response);
+  } catch (err) {
+    showError(err);
+  }
 }
 
-async function initiateRedirect(formAsJson, pmMethodType) {
-	try {
-		displayResult("Processing Payment with Worldline.", "");
-		if (pmMethodType == 'ibp') {
-			var response = await service.redirectPayment().paymentForm(document.getElementById("online_banking_details"), 'data-ibp').send()
-		}
-		else if (pmMethodType == 'ewallet') {
-			var response = await service.redirectPayment().paymentForm(document.getElementById("ewallet_details"), 'data-ewallet').send()
-		}
-		if (response.bankUrl) {
-			displayResult("Redirecting to bank's site.", "");
-			processRedirect(response);
-		}
-		else {
-			//unpack response
-			unpackResponse(response);
-		}
-	}
-	catch (err) {
-		showError(err);
-	}
+async function initiateRedirect(pmMethodType) {
+  try {
+    displayResult('Processing Payment with Worldline.', '');
+    if (pmMethodType == 'ibp') {
+      var response = await service
+        .redirectPayment()
+        .paymentForm(document.getElementById('online_banking_details'), 'data-ibp')
+        .send();
+    } else if (pmMethodType == 'ewallet') {
+      var response = await service
+        .redirectPayment()
+        .paymentForm(document.getElementById('ewallet_details'), 'data-ewallet')
+        .send();
+    }
+    if (response.bankUrl) {
+      displayResult("Redirecting to bank's site.", '');
+      processRedirect(response);
+    } else {
+      //unpack response
+      unpackResponse(response);
+    }
+  } catch (err) {
+    showError(err);
+  }
 }
 
-async function processEft(formAsJson) {
-	try {
-		displayResult("Processing Payment with Worldline.", "");
-		var response = await service.eftPayment().paymentForm(document.getElementById("eft_details"), 'data-eft').send();
-		displayResult("Processing result with merchant.", "");
-		unpackResponse(response);
-	}
-	catch (err) {
-		showError(err);
-	}
+async function processEft() {
+  try {
+    displayResult('Processing Payment with Worldline.', '');
+    var response = await service
+      .eftPayment()
+      .paymentForm(document.getElementById('eft_details'), 'data-eft')
+      .send();
+    displayResult('Processing result with merchant.', '');
+    unpackResponse(response);
+  } catch (err) {
+    showError(err);
+  }
 }
 
 function timeout(ms) {
-	return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function getPaymentMethods() {
-	var response = await makeRequest({
-		method: 'GET',
-		url: '/api/demo/paymentMethodEndPoint'
-	})
-	try {
-		var data = JSON.parse(response);
-		service = new Worldline.PaymentService(data);
-		var paymentMethods = await service.getIbpPaymentMethods().send();
-		populateOptions("ibpList", paymentMethods);
-		
-		paymentMethods = await service.getEftPaymentMethods().send();
-		populateOptions("eftList", paymentMethods);
-		
-		paymentMethods = await service.getEWalletPaymentMethods().send();
-		populateOptions("ewalletList", paymentMethods);
-				
-	} catch (err) {
-		showError(err);
-	}		
+  var response = await makeRequest({
+    method: 'GET',
+    url: '/api/demo/paymentMethodEndPoint'
+  });
+  try {
+    var data = JSON.parse(response);
+    service = new Worldline.PaymentService(data);
+    var paymentMethods = await service.getIbpPaymentMethods().send();
+    populateOptions('ibpList', paymentMethods);
+
+    paymentMethods = await service.getEftPaymentMethods().send();
+    populateOptions('eftList', paymentMethods);
+
+    paymentMethods = await service.getEWalletPaymentMethods().send();
+    populateOptions('ewalletList', paymentMethods);
+  } catch (err) {
+    showError(err);
+  }
 }
 
 function populateOptions(paymentMethodList, paymentMethods) {
-	paymentMethods.forEach(function (paymentMethod) {
-		var list = document.getElementById(paymentMethodList);
-		var opt = document.createElement('option');
-		opt.value = paymentMethod.id;
-		opt.text = paymentMethod.name;
-		list.options.add(opt);
-	});
+  paymentMethods.forEach(function(paymentMethod) {
+    var list = document.getElementById(paymentMethodList);
+    var opt = document.createElement('option');
+    opt.value = paymentMethod.id;
+    opt.text = paymentMethod.name;
+    list.options.add(opt);
+  });
 }
-	
-window.addEventListener('message', function (e) {
-	var key = e.message ? 'message' : 'data';
-	var wlResponse = JSON.parse(e[key]);
 
-	if ('AUTHENTICATION' === wlResponse.type) {
-		var params = wlResponse.parameters;
-		if ('SUCCESSFUL' === params.authenticationStatus) {
-			displayResult("Authentication Successful. Proceed with Payment", "");
-			processCardAfterAuthentication(deviceAPIRequest, params.worldlineSessionData);
-		} else {
-			//Don't proceed for payment flow and show appropriate message to user.
-			displayResult("Order Id: " + params.orderId
-				+ "<br>Authentication Status Description: " + params.authenticationStatusDescription
-				+ "<br>Authentication Status: " + params.authenticationStatus
-				, "");
-		}
-	} else {
-		unpackResponse(wlResponse);
-	}
+window.addEventListener(
+  'message',
+  function(e) {
+    var key = e.message ? 'message' : 'data';
+    var wlResponse = JSON.parse(e[key]);
 
-	var redirectDiv = document.getElementById('iframeDiv');
-	redirectDiv.removeChild(iframeDiv.childNodes[0]);
-	console.log('data', wlResponse);
-
-}, false);
+    if ('AUTHENTICATION' === wlResponse.type) {
+      var params = wlResponse.parameters;
+      if ('SUCCESSFUL' === params.authenticationStatus) {
+        displayResult('Authentication Successful. Proceed with Payment', '');
+        processCardAfterAuthentication(deviceAPIRequest, params.worldlineSessionData);
+      } else {
+        //Don't proceed for payment flow and show appropriate message to user.
+        displayResult(
+          'Order Id: ' +
+            params.orderId +
+            '<br>Authentication Status Description: ' +
+            params.authenticationStatusDescription +
+            '<br>Authentication Status: ' +
+            params.authenticationStatus,
+          ''
+        );
+      }
+    } else {
+      unpackResponse(wlResponse);
+    }
+    var redirectDiv = document.getElementById('iframeDiv');
+    redirectDiv.removeChild(iframeDiv.childNodes[0]);
+  },
+  false
+);
